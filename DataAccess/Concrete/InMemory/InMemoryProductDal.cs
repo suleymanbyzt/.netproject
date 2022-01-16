cbe3d344cb396c949a4fa0dcccbab63f709247d2
Suleyman Bayazit <suleymanbayazitt@gmail.com>
2022-01-16 17:59:33 +0300

    Entity Framework used

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.InMemory
 {
@@ -14,12 +13,13 @@ namespace DataAccess.Concrete.InMemory
         List<Product> _products;
         public InMemoryProductDal()
         {
-            _products = new List<Product> { 
-                new Product{ProductId=1,CategoryId=1, ProductName="Bardak", UnitPrice =15, UnitsInStock=15},
-                new Product{ProductId=2,CategoryId=1, ProductName="Kamera", UnitPrice =500, UnitsInStock=3},
-                new Product{ProductId=3,CategoryId=2, ProductName="Telefon", UnitPrice =1500, UnitsInStock=2},
-                new Product{ProductId=4,CategoryId=2, ProductName="Klavye", UnitPrice =150, UnitsInStock=65},
-                new Product{ProductId=5,CategoryId=2, ProductName="Fare", UnitPrice =85, UnitsInStock=1},
+            //Oracle,Sql Server, Postgres , MongoDb
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
             };
         }
         public void Add(Product product)
@@ -29,13 +29,11 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Product product)
         {
-            Product productToDelete;
-            productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
-        }
+            //LINQ - Language Integrated Query
+            //Lambda
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
 
-        public Product Get(Expression<Func<Product, bool>> filter = null)
-        {
-            throw new NotImplementedException();
+            _products.Remove(productToDelete);
         }
 
         public List<Product> GetAll()
@@ -43,9 +41,14 @@ namespace DataAccess.Concrete.InMemory
             return _products;
         }
 
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        public void Update(Product product)
         {
-            throw new NotImplementedException();
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
 
         public List<Product> GetAllByCategory(int categoryId)
@@ -53,14 +56,14 @@ namespace DataAccess.Concrete.InMemory
             return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
-        public void Update(Product product)
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
-            productToUpdate.ProductName = product.ProductName;
-            productToUpdate.CategoryId = product.CategoryId;
-            productToUpdate.UnitPrice = product.UnitPrice;
-            productToUpdate.UnitsInStock = product.UnitsInStock;
+            throw new NotImplementedException();
+        }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
